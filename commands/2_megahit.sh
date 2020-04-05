a2f57bdee20101b0f4ea0d0054e6bc97b4ff4431
Cris <cristinadiazubieta@gmail.com>
2020-04-05 12:15:44 +0200

    changed 2_megahit.sh

--- commands/2_megahit.sh
+++ commands/2_megahit.sh
@@ -7,9 +7,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user cristinadiazubieta@gmail.com
 # Load modules
-
 module load bioinfo-tools
 module load megahit
+
 # Your commands
 megahit –kmin-1pass -t 2 -o /home/crisdu/GA/analyses_done/2_megahit/DNA_trimmed_megahit /home/crisdu/4_Thrash_2017/DNA_trimmed/*.fastq.gz
-megahit –kmin-1pass -t 2 -o /home/crisdu/GA/analyses_done/2_megahit/RNA_untrimmed_megahit /home/crisdu/4_Thrash_2017/RNA_untrimmed/*.fastq.gz
+

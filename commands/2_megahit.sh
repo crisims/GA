6ebd4ff3bb4db36a24b2f652124d32a760e6b9c4
Cris <cristinadiazubieta@gmail.com>
2020-04-14 11:33:29 +0200

    final version of the 2_megahit.sh

--- commands/2_megahit.sh
+++ commands/2_megahit.sh
@@ -6,11 +6,14 @@
 #SBATCH -J megahit_analysis
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user cristinadiazubieta@gmail.com
+
 # Load modules
 module load bioinfo-tools
 module load megahit
 
 # Your commands
-megahit –kmin-1pass -t 2 -o /home/crisdu/GA/analyses_done/2_megahit/DNA_trimmed_megahit /home/crisdu/4_Thrash_2017/DNA_trimmed/*.fastq.gz
-megahit –kmin-1pass -t 2 -o /home/crisdu/GA/analyses_done/2_megahit/RNA_untrimmed_megahit /home/crisdu/4_Thrash_2017/RNA_untrimmed/*.fastq.gz
+megahit -1 ~/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz -2 ~/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz 
+--kmin-1pass --k-min 65 --k-max 105 --k-step 10 -t 2 -o /home/crisdu/GA/analyses_done/2_megahit
+megahit -1 ~/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz -2 ~/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz 
+--kmin-1pass --k-min 65 --k-max 105 --k-step 10 -t 2 -o /home/crisdu/GA/analyses_done/2_megahit
 

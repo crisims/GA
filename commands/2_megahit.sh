4ff1935e2cde278b46b5c8de6472be0ca944c654
Cris <cristinadiazubieta@gmail.com>
2020-04-15 13:47:00 +0200

    batch job..

--- commands/2_megahit.sh
+++ commands/2_megahit.sh
@@ -12,6 +12,5 @@ module load bioinfo-tools
 module load megahit/1.1.2
 
 # Your commands
-megahit -1 ~/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz -2 ~/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz --kmin-1pass -t 2 -o /home/crisdu/GA/analyses_done/2_megahit/results_pair1
-megahit -1 ~/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz -2 ~/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz --kmin-1pass -t 2 -o /home/crisdu/GA/analyses_done/2_megahit/results_pair2
+megahit -1 /proj/g2020008/4_Thrash_2017/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz -2 /proj/g2020008/4_Thrash_2017/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz --kmin-1pass --k-min 65 --k-max 105 --k-step 10 -t 2 -o /home/crisdu/GA/analyses_done/2_megahit/results_pair1
 

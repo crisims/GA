6bdc41bc78a5751da42bca4b4edbbb15e8ce81f4
Cris <cristinadiazubieta@gmail.com>
2020-04-22 14:09:19 +0200

    added the script for metabat

---
+++ commands/3_metabat.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:01:00
+#SBATCH -J metabat_analysis
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user cristinadiazubieta@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load MetaBat/2.12.1
+# Your commands
+
+metabat2 -i /home/crisdu/analyses_done/2_megahit/concatenated_contigs.fa -o /home/crisdu/analyses_done/3_metabat -t 2
+
+#metabat output files will have a dot in their name, change the dot before running CheckM!!

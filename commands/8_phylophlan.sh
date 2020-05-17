9011fcbdd6b314be6f05571c6616391e38418860
Cris <cristinadiazubieta@gmail.com>
2020-05-17 12:34:16 +0200

    changed name of files

--- commands/8_phylophlan.sh
+++ commands/8_phylophlan.sh
@@ -11,13 +11,12 @@
 module load bioinfo-tools
 module load  phylophlan/0.99
 module load  python/2.7.11
-module load  biopython/1.68-py3
 module load  FastTree/2.1.10
 module load  muscle/3.8.1551
 module load  usearch/5.2.32
 
 # set this to the directory where you want to store your phylophlan results
-outdir = /home/crisdu/analyses_done/6_phylo
+outdir = /home/crisdu/analyses_done/8_phylo
 # create the directories that phylophlan looks for:
 # when you run phylophlan you specify a project name, which it looks for in the "input" directory
 # in this case the project name is "metagenome"
@@ -32,10 +31,12 @@ ln -sf /sw/apps/bioinfo/phylophlan/0.99/rackham/bin/data/ppafull.tax.txt $outdir
 ln -sf /sw/apps/bioinfo/phylophlan/0.99/rackham/bin/taxcuration/ $outdir/taxcuration
 
 # symlinks to input files, NOT FINISHED!!
-ln -sf /home/crisdu/analyses_done/5_prokka/bin_1/annotation_bin1.faa $outdir/input/metagenome
-ln -sf /home/crisdu/analyses_done/5_prokka/bin_2/annotation_bin2.faa $outdir/input/metagenome
-ln -sf /home/crisdu/analyses_done/5_prokka/bin_3/annotation_bin3.faa $outdir/input/metagenome
-ln -sf /home/crisdu/analyses_done/5_prokka/bin_4/annotation_bin4.faa $outdir/input/metagenome
+bin=(1 2 3 6 8 9 14 17 18 19 20 21 22 23 24 25)
+
+for i in "${bin[@]}"
+do
+    ln -sf /home/crisdu/analyses_done/5_prokka/bin_$i/annotation_bin$i.faa $outdir/input/metagenome
+done
 
 # Your commands
 cd $outdir
